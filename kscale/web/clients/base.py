d21f39f83e9fea0a7abb56b45f09f0e435ccd4ae
Benjamin Bolte <ben@bolte.cc>
2025-01-16 12:31:40 -0800

    lint

--- kscale/web/clients/base.py
+++ kscale/web/clients/base.py
@@ -15,8 +15,7 @@ import aiohttp
 import httpx
 from aiohttp import web
 from async_lru import alru_cache
-from jwt import ExpiredSignatureError, PyJWKClient
-from jwt import decode as jwt_decode
+from jwt import ExpiredSignatureError, PyJWKClient, decode as jwt_decode
 from pydantic import BaseModel
 from yarl import URL
 
